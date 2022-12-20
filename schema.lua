0f17b6ef548004a39f066e6267bf0e6fec73ca2b
deirdre-anderson <deirdre.anderson813@gmail.com>
2022-12-20 12:59:01 -0800

    Updated plugin name in schema.lua

--- schema.lua
+++ schema.lua
@@ -9,7 +9,7 @@ local typedefs = require "kong.db.schema.typedefs"
 
 
 return {
-  name = "file-log",
+  name = "debug-file-log",
   fields = {
     { protocols = typedefs.protocols },
     { config = {
